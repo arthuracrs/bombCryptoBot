6931e474629b4094c04f61aded346eeed3a8024d
arthur <arthuracrs3@gmail.com>
2022-01-17 10:34:06 -0300

    add log files

--- main.py
+++ main.py
@@ -5,22 +5,34 @@ import datetime
 import time
 import random
 
+
 def clearPrint(text):
     os.system('cls')
     print(text)
+
 
 pyautogui.PAUSE = 1
 
 last_work = datetime.datetime.now()
 last_idle_move = datetime.datetime.now()
 
+directory = './logs'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+logFile = open(
+    directory + "/log-"+datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")+".txt", "x")
+logs = []
+
+
+
 state = 'disconnected'
 lastState = state
 connected = ''
 loggedIn = True
 
-restTime = 30 * 60 + random.randint(1,9)
-idleMovementTimer = 4 * 60 + random.randint(1,9)
+restTime = 30 * 60 + random.randint(1, 9)
+idleMovementTimer = 4 * 60 + random.randint(1, 9)
 
 clearPrint("It's on baby!")
 
@@ -36,24 +48,29 @@ while connected != 's':
         input('Precione Enter para continuar')
         os.system('cls')
 
+
 def customClick(coords):
     pyautogui.moveTo(coords.x, coords.y, 1)
     pyautogui.click(clicks=2, interval=0.25)
 
+
 def connectWallet(currentState):
     global loggedIn
-    connect_wallet_im1 = pyautogui.locateCenterOnScreen('connect_wallet_01.png', confidence=0.7)
+    connect_wallet_im1 = pyautogui.locateCenterOnScreen(
+        'connect_wallet_01.png', confidence=0.7)
     if connect_wallet_im1 != None:
         customClick(connect_wallet_im1)
         return 'to_asign'
-        
+
     else:
-     connect_wallet_im2 = pyautogui.locateCenterOnScreen('connect_wallet_02.png', confidence=0.7)
-     if connect_wallet_im2 != None:
-        customClick(connect_wallet_im2)
-        return 'to_asign'
-    
+        connect_wallet_im2 = pyautogui.locateCenterOnScreen(
+            'connect_wallet_02.png', confidence=0.7)
+        if connect_wallet_im2 != None:
+            customClick(connect_wallet_im2)
+            return 'to_asign'
+
     return currentState
+
 
 def asign(currentState):
     global loggedIn
@@ -64,6 +81,7 @@ def asign(currentState):
         return 'to_expan'
 
     return currentState
+
 
 def reAsign(currentState):
     global loggedIn
@@ -82,12 +100,15 @@ def reAsign(currentState):
 
     return currentState
 
+
 def expandGameWindow(currentState):
-    expand_game_Window_im1 = pyautogui.locateCenterOnScreen('expan_game.png', confidence=0.8)
+    expand_game_Window_im1 = pyautogui.locateCenterOnScreen(
+        'expan_game.png', confidence=0.8)
     if expand_game_Window_im1 != None:
         customClick(expand_game_Window_im1)
         return 'to_heroes'
     return currentState
+
 
 def openHeroesWindow(currentState):
     heroes_im1 = pyautogui.locateCenterOnScreen('heroes.png', confidence=0.8)
@@ -96,6 +117,7 @@ def openHeroesWindow(currentState):
         return 'to_all_work'
     return currentState
 
+
 def closeHeroesWindow(currentState):
     close_im1 = pyautogui.locateCenterOnScreen('close.png', confidence=0.7)
     if close_im1 != None:
@@ -103,68 +125,80 @@ def closeHeroesWindow(currentState):
         return 'to_treasure_hunt'
     return currentState
 
+
 def putAllHeroesToWork(currentState):
     if pyautogui.locateOnScreen('all_on.png') != None:
-        all_on_im1 = pyautogui.locateCenterOnScreen('all_on.png', confidence=0.8)
+        all_on_im1 = pyautogui.locateCenterOnScreen(
+            'all_on.png', confidence=0.8)
         customClick(all_on_im1)
         return 'to_close'
 
     elif pyautogui.locateOnScreen('all_work_again.png') != None:
-        all_work_again_im1 = pyautogui.locateCenterOnScreen('all_work_again.png')
+        all_work_again_im1 = pyautogui.locateCenterOnScreen(
+            'all_work_again.png')
         customClick(all_work_again_im1)
         return 'to_close'
-        
+
     else:
         print('vazei')
         return 'to_close'
 
     return currentState
 
+
 def backToMenu(currentState):
-    back_menu_im1 = pyautogui.locateCenterOnScreen('back_menu.png', confidence=0.8)
+    back_menu_im1 = pyautogui.locateCenterOnScreen(
+        'back_menu.png', confidence=0.8)
     if back_menu_im1 != None:
         customClick(back_menu_im1)
         return 'to_heroes'
     return currentState
 
+
 def goTreasureHunt(currentState):
-    treasure_hunt_im1 = pyautogui.locateCenterOnScreen('treasure_hunt.png', confidence=0.8)
+    treasure_hunt_im1 = pyautogui.locateCenterOnScreen(
+        'treasure_hunt.png', confidence=0.8)
     if treasure_hunt_im1 != None:
         customClick(treasure_hunt_im1)
         return 'farming'
     return currentState
 
+
 def idleMovement(currentState):
     global last_idle_move
-    idle_target_im1 = pyautogui.locateCenterOnScreen('chest.png', confidence=0.8)
+    idle_target_im1 = pyautogui.locateCenterOnScreen(
+        'chest.png', confidence=0.8)
     if idle_target_im1 != None:
         customClick(idle_target_im1)
-        time.sleep(random.randint(1,9))
-        idle_target_2_im1 = pyautogui.locateCenterOnScreen('close_chest.png', confidence=0.8)
+        time.sleep(random.randint(1, 9))
+        idle_target_2_im1 = pyautogui.locateCenterOnScreen(
+            'close_chest.png', confidence=0.8)
         if idle_target_2_im1 != None:
             customClick(idle_target_2_im1)
-        
+
             last_idle_move = datetime.datetime.now()
-        
+
             return 'farming'
     return currentState
+
 
 def checkIfDisconnected():
     global state
     global loggedIn
-    
+
     if pyautogui.locateOnScreen('login_expired.png', confidence=0.8) != None:
         ok_im1 = pyautogui.locateCenterOnScreen('ok.png', confidence=0.8)
         if ok_im1 != None:
             customClick(ok_im1)
             state = 'disconnected'
-        
+
         ok_im2 = pyautogui.locateCenterOnScreen('ok2.png', confidence=0.8)
         if ok_im2 != None:
             customClick(ok_im2)
             state = 'disconnected'
     elif loggedIn:
-        connect_wallet_02_im = pyautogui.locateCenterOnScreen('connect_wallet_02.png', confidence=0.8)
+        connect_wallet_02_im = pyautogui.locateCenterOnScreen(
+            'connect_wallet_02.png', confidence=0.8)
         if connect_wallet_02_im != None:
             print('deu ruim')
             loggedIn = False
@@ -174,46 +208,55 @@ def checkIfDisconnected():
             time.sleep(10)
             state = 'to_re_asign'
 
+
 while 1:
+    log = '%s ||    %s' % (
+        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), state)
     if lastState != state:
-        print('%s ||    %s' % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), state))
+        print(log)
+        logs.append(log)
 #     clearPrint('''LoggedIn: %r
 # Atividade Atual: %s
 # Proximo movimento de presença: %ds
 # Fim do descanço em: %dm''' % (loggedIn, state,  idleMovementTimer - (datetime.datetime.now() - last_idle_move).seconds, (restTime - (datetime.datetime.now() - last_work).seconds)/ 60))
-    
+
     lastState = state
+
+    if keyboard.is_pressed('q') and keyboard.is_pressed('ctrl'):
+        print('You Pressed A Key!')
+
+        break  # finishing the loop
 
     if state == 'disconnected':
         state = connectWallet(state)
 
     elif state == 'to_asign':
         state = asign(state)
-    
+
     elif state == "to_re_asign":
         state == reAsign(state)
-    
+
     elif state == 'to_expan':
         state = expandGameWindow(state)
 
     elif state == 'to_heroes':
         state = openHeroesWindow(state)
-    
+
     elif state == 'to_all_work':
         state = putAllHeroesToWork(state)
-    
+
     elif state == 'to_close':
         state = closeHeroesWindow(state)
-    
+
     elif state == 'to_treasure_hunt':
         state = goTreasureHunt(state)
-    
+
     elif state == 'back_menu':
         state = backToMenu(state)
 
     elif state == 'idle_move':
         state = idleMovement(state)
-    
+
     elif state == 'farming' and (datetime.datetime.now() - last_work).seconds > restTime:
         state = 'back_menu'
 
@@ -221,4 +264,8 @@ while 1:
         state = 'idle_move'
 
     checkIfDisconnected()
-    time.sleep(1)
+    # time.sleep(1)
+
+for x in logs:
+    logFile.write(x)
+logFile.close()
